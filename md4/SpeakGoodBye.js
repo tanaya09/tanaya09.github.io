03747d58063c04c6faf04a0384871507bccac83b
tanaya09 <89646541+tanaya09@users.noreply.github.com>
2023-07-05 19:41:13 +0530

    update

--- md4/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-// speakgoodbye.js
-
-var byeSpeaker = {}; // Create the byeSpeaker object
-
-var speakWord = "Good Bye";
-
-byeSpeaker.speak = function(name) {
-  console.log(speakWord + " " + name);
-};
