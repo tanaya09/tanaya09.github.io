61b55048d4bb42a973c6ed8127ea96c654659946
tanaya09 <89646541+tanaya09@users.noreply.github.com>
2023-07-05 11:47:20 +0530

    update

---
+++ md4/SpeakGoodBye.js
@@ -0,0 +1,9 @@
+// speakgoodbye.js
+
+var byeSpeaker = {}; // Create the byeSpeaker object
+
+var speakWord = "Good Bye";
+
+byeSpeaker.speak = function(name) {
+  console.log(speakWord + " " + name);
+};
---
+++ md4/SpeakHello.js
@@ -0,0 +1,7 @@
+var helloSpeaker = {}; // Create the helloSpeaker object
+
+var speakWord = "Hello";
+
+helloSpeaker.speak = function(name) {
+  console.log(speakWord + " " + name);
+};
